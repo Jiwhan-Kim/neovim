c5ee9470b5d58429b6303b8af5ff86ab670a0fb5
Jiwhan Kim <97083644+Jiwhan-Kim@users.noreply.github.com>
2025-06-27 14:31:51 +0900

    Add LSP and formatter plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,41 @@
+return {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        { 'williamboman/mason.nvim', config = true },
+        { 'williamboman/mason-lspconfig.nvim' },
+    },
+    config = function()
+        require('mason').setup()
+        require('mason-lspconfig').setup {
+            ensure_installed = { 'lua_ls' },
+        }
+
+        local lspconfig = require('lspconfig')
+
+        local on_attach = function(_, bufnr)
+            local nmap = function(keys, func, desc)
+                if desc then
+                    desc = 'LSP: ' .. desc
+                end
+                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+            end
+
+            nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+            nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+            nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+            nmap('<leader>f', function()
+                vim.lsp.buf.format { async = true }
+            end, '[F]ormat buffer')
+        end
+
+        require('mason-lspconfig').setup_handlers {
+            function(server)
+                lspconfig[server].setup {
+                    on_attach = on_attach,
+                }
+            end,
+        }
+    end,
+}
